1fe6edfd1dfb5698a86acd19d6c679708c4b1760
Cristian Diaz <43429236+KrizPLAY@users.noreply.github.com>
2020-02-25 12:41:07 -0600

    update

--- app/src/main/java/com/emman/votacionesido2020/splashActivity.java
+++ app/src/main/java/com/emman/votacionesido2020/splashActivity.java
@@ -2,13 +2,33 @@ package com.emman.votacionesido2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.emman.votacionesido2020.ui.login.LoginActivity;
 
 public class splashActivity extends AppCompatActivity {
 
+    private final int DURACION_SPLASH = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(splashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
     }
 }
